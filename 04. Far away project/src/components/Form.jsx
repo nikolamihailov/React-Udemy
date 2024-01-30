32a49471ed42b792c1509da182508208d033d706
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-01-30 23:52:51 +0200

    made form controllable

--- 04. Far away project/src/components/Form.jsx
+++ 04. Far away project/src/components/Form.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+  };
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={onFormSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, idx) => idx + 1).map((el) => {
+          return (
+            <option value={el} key={el}>
+              {el}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
