6a6cc1f1d6b4f81c0cbd9dcd25ba3637fa9cdbf0
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-02-03 20:53:03 +0200

    moved func declaration

--- 06. Usepopcorn project/src/components/WatchedSummary.jsx
+++ 06. Usepopcorn project/src/components/WatchedSummary.jsx
@@ -1,6 +1,7 @@
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
 export default function WatchedSummary({ watched }) {
-  const average = (arr) =>
-    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
