3fb598a9f849fd337fde1b4e3a2ffb2ef5e322c4
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-02-12 12:23:16 +0200

    problem with key fixed, section finished

--- 05. Eat-n-split/src/App.jsx
+++ 05. Eat-n-split/src/App.jsx
@@ -68,6 +68,7 @@ function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
