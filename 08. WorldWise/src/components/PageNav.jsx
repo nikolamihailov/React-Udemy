91f821e5652621c65235028ca38d963ff46343e9
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-02-22 22:53:01 +0200

    starter components added

--- 08. WorldWise/src/components/PageNav.jsx
+++ 08. WorldWise/src/components/PageNav.jsx
@@ -1,18 +1,20 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
     <nav className={styles["nav"]}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to={"/"}>Home</NavLink>
-        </li>
         <li>
           <NavLink to={"/pricing"}>Pricing</NavLink>
         </li>
         <li>
           <NavLink to={"/product"}>Product</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/login"}>Login</NavLink>
         </li>
       </ul>
     </nav>
--- 08. WorldWise/src/pages/HomePage.jsx
+++ 08. WorldWise/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
+import PageNav from "../components/PageNav";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
+      <PageNav />
       <section>
         <h1>
           You travel the world.
