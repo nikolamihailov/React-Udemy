7698715972b99c5856cd2d4a2460803e5ddb1770
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-02-29 23:12:21 +0200

    fake auth context added

---
+++ 08. WorldWise/src/contexts/FakeAuthentication.jsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useReducer } from "react";
+
+const FakeAuthenticationContext = createContext();
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+const initialValues = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("No such action");
+  }
+};
+
+export const FakeAuthenticationProvider = function ({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialValues
+  );
+  const login = (email, password) => {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  };
+  const logout = () => {
+    dispatch({ type: "logout" });
+  };
+  return (
+    <FakeAuthenticationContext.Provider
+      values={(login, logout, user, isAuthenticated)}
+    >
+      {children}
+    </FakeAuthenticationContext.Provider>
+  );
+};
+
+export const useAuth = function () {
+  const context = useContext(FakeAuthenticationContext);
+  if (context === undefined) throw new Error("Using ctx out of scope");
+  return context;
+};
