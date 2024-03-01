6d4897c5c5bacc49237a9889cfbfd2fbad92987f
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-01 18:21:59 +0200

    optimizating re-rendeing of slow component using the children prop added

--- 09. Atomic blog/src/App.js
+++ 09. Atomic blog/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import PostProvider, { usePosts } from "./PostContext";
 import { faker } from "@faker-js/faker";
+import Test from "./Test";
 
 function createRandomPost() {
   return {
@@ -130,14 +131,18 @@ function List() {
   const { posts } = usePosts();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      <Test />
+    </>
   );
 }
 
