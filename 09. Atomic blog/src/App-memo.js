49e0f3be7ef5aeff81bc3568de57cd127b8320e7
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-01 19:24:42 +0200

    showing usecase of usememo added

--- 09. Atomic blog/src/App-memo.js
+++ 09. Atomic blog/src/App-memo.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -41,6 +41,13 @@ function App() {
     [isFakeDark]
   );
 
+  const archiveOptions = useMemo(() => {
+    return {
+      show: false,
+      title: "Some title"
+    };
+  }, []);
+
   return (
     <section>
       <button
@@ -57,7 +64,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive show={false} />
+      <Archive archiveOptions={archiveOptions} />
       <Footer />
     </section>
   );
@@ -155,18 +162,18 @@ function List({ posts }) {
 
 }
 
-const Archive = memo(function Archive({ show }) {
+const Archive = memo(function Archive({ archiveOptions }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(show);
+  const [showArchive, setShowArchive] = useState(archiveOptions.show);
 
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveOptions.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
