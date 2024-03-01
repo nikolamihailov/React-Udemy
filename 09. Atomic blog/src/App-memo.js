c95997080668055312176d8bfbffe52792901631
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-01 19:27:45 +0200

    added dependecny array to usememo

--- 09. Atomic blog/src/App-memo.js
+++ 09. Atomic blog/src/App-memo.js
@@ -44,9 +44,9 @@ function App() {
   const archiveOptions = useMemo(() => {
     return {
       show: false,
-      title: "Some title"
+      title: `Post archive in addition to ${posts.length} main posts`
     };
-  }, []);
+  }, [posts.length]);
 
   return (
     <section>
