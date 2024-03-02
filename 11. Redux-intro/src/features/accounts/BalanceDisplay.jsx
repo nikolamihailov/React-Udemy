841a092db9107591f4762ead5beb0b2299e656d6
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-02 19:05:45 +0200

    legacy way of connected components to redux added to balance component

--- 11. Redux-intro/src/features/accounts/BalanceDisplay.jsx
+++ 11. Redux-intro/src/features/accounts/BalanceDisplay.jsx
@@ -1,3 +1,5 @@
+import { connect } from "react-redux";
+
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -5,8 +7,15 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+// eslint-disable-next-line react/prop-types
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
