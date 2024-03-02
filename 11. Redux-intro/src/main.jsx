71543dd98772be65ffd4f47f3e6b1058572ca760
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-02 18:12:07 +0200

    connected react app with the react redux provider and passed store

--- 11. Redux-intro/src/main.jsx
+++ 11. Redux-intro/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import store from "./store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
