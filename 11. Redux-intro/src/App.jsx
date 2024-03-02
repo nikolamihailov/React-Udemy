9cb8b9c9cfaf46a4402168a12757993b9b42bd63
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-02 18:25:56 +0200

    conditional rendering of app comonents based on existing user bumped

--- 11. Redux-intro/src/App.jsx
+++ 11. Redux-intro/src/App.jsx
@@ -2,15 +2,22 @@ import Customer from "./features/customers/Customer";
 import CreateCustomer from "./features/customers/CreateCustomer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((store) => store.customer.fullName);
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {fullName === "" ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
