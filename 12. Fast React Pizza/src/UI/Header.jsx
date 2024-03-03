c177e0052f65f09033aad2a9f2b982e1e16030ef
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-03 18:40:55 +0200

    built the app layout

---
+++ 12. Fast React Pizza/src/UI/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza</Link>
+      <p>Koleto</p>
+    </header>
+  );
+}
+
+export default Header;
