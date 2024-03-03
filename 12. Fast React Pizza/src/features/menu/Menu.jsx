b2ed2e2079527d40a2c26ca46899fd6091c6e686
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-03 18:56:46 +0200

    implemented render as you fetch method with react router loader

--- 12. Fast React Pizza/src/features/menu/Menu.jsx
+++ 12. Fast React Pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,22 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  console.log(menu);
+  return (
+    <ul>
+      {menu.map((item) => {
+        return <MenuItem pizza={item} key={item.id} />;
+      })}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
