173e34ef8aeeca350aa7a6e998d07ccf1a7427bc
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-03 19:10:52 +0200

    handled errors using the error element from react router

--- 12. Fast React Pizza/src/UI/Error.jsx
+++ 12. Fast React Pizza/src/UI/Error.jsx
@@ -1,12 +1,13 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from "react-router-dom";
 
 function NotFound() {
   const navigate = useNavigate();
+  const err = useRouteError();
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{err.data || err.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
