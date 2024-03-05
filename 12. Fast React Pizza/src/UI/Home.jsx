08d1217b80611e9bdf9b56370cfa34eb4da3600b
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-05 12:52:11 +0200

    first classes added

--- 12. Fast React Pizza/src/UI/Home.jsx
+++ 12. Fast React Pizza/src/UI/Home.jsx
@@ -1,13 +1,13 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-center text-xl font-semibold text-yellow-500 ">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
       </h1>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
