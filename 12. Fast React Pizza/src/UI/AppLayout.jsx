0c0b063726800958766c8848aa20925216461942
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-05 16:56:09 +0200

    grid model demo bumped

--- 12. Fast React Pizza/src/UI/AppLayout.jsx
+++ 12. Fast React Pizza/src/UI/AppLayout.jsx
@@ -1,24 +1,25 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from "react-router-dom"
+import CartOverview from "../features/cart/CartOverview"
+import Header from "./Header"
+import Loader from "./Loader"
 
 function AppLayout() {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const navigation = useNavigation()
+  const isLoading = navigation.state === "loading"
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
-
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-auto">
+        <main className=" mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
-  );
+  )
 }
 
-export default AppLayout;
+export default AppLayout
