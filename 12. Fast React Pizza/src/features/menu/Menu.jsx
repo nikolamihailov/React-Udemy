d9939153b71a3a859d65d91830441e8d58483911
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-05 22:54:01 +0200

    styled the menu page

--- 12. Fast React Pizza/src/features/menu/Menu.jsx
+++ 12. Fast React Pizza/src/features/menu/Menu.jsx
@@ -5,7 +5,7 @@ import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((item) => {
         return <MenuItem pizza={item} key={item.id} />;
       })}
--- 12. Fast React Pizza/src/features/menu/MenuItem.jsx
+++ 12. Fast React Pizza/src/features/menu/MenuItem.jsx
@@ -1,16 +1,31 @@
 import { formatCurrency } from "../../utils/helpers";
+import Button from "../../UI/Button";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "grayscale opacity-70" : ""}`}
+      />
+      <div className="flex flex-col grow pt-1">
+        <p className="font-medium">{name}</p>
+        <p className="capitalize text-sm italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-start justify-between">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
+
+          <Button type="small">Add to cart</Button>
         </div>
       </div>
     </li>
