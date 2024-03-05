9669c2fec93a357b3c2070e24838e4f995cd27eb
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-05 14:08:29 +0200

    flexbox examples added

--- 12. Fast React Pizza/src/features/user/Username.jsx
+++ 12. Fast React Pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Koleto</div>
+  return <div className=" hidden text-sm font-semibold md:block">Koleto</div>
 }
 
 export default Username
