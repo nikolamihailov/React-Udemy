0ee13654f64422816b219914d96f6eb95efc3cab
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-05 22:35:56 +0200

    changed default font-family and experimented with tailwind config

--- 12. Fast React Pizza/src/UI/AppLayout.jsx
+++ 12. Fast React Pizza/src/UI/AppLayout.jsx
@@ -7,7 +7,7 @@ function AppLayout() {
   const navigation = useNavigation()
   const isLoading = navigation.state === "loading"
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+    <div className="grid h-dvh grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
--- 12. Fast React Pizza/tailwind.config.js
+++ 12. Fast React Pizza/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    extend: {
+      /*  fontSize: {
+         huge: ['80rem', { lineHeight: '1' }],
+       },
+       height: {
+         screen: '100dvh',
+       }, */
+    },
   },
   plugins: [],
-}
-
+};
