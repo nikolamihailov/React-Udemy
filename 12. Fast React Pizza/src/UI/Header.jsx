e4df6034d2f47d89d55be3b137e1881371fdae95
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-05 13:02:55 +0200

    working with colors and shades demo added

--- 12. Fast React Pizza/src/UI/Header.jsx
+++ 12. Fast React Pizza/src/UI/Header.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from "react-router-dom"
+import SearchOrder from "../features/order/SearchOrder"
 
 function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza</Link>
       <SearchOrder />
       <p>Koleto</p>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
--- 12. Fast React Pizza/src/features/cart/CartOverview.jsx
+++ 12. Fast React Pizza/src/features/cart/CartOverview.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="bg-stone-800 text-stone-200">
+      <p className="text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
-  );
+  )
 }
 
-export default CartOverview;
+export default CartOverview
