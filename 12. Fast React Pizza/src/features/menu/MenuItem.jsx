ad707cd1d4406147fd3b2c20ae38919da9cc726e
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-06 19:35:53 +0200

    made the add to cart button work

--- 12. Fast React Pizza/src/features/menu/MenuItem.jsx
+++ 12. Fast React Pizza/src/features/menu/MenuItem.jsx
@@ -1,19 +1,33 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../UI/Button";
+import { addItem } from "../cart/cartSlice";
+import { useDispatch } from "react-redux";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    const item = {
+      pizzaId: id,
+      name,
+      unitPrice,
+      quantity: 1,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(item));
+  };
 
   return (
     <li className="flex gap-4 py-2">
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? "grayscale opacity-70" : ""}`}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
       />
-      <div className="flex flex-col grow pt-1">
+      <div className="flex grow flex-col pt-1">
         <p className="font-medium">{name}</p>
-        <p className="capitalize text-sm italic text-stone-500">
+        <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(", ")}
         </p>
         <div className="mt-auto flex items-start justify-between">
@@ -25,7 +39,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
