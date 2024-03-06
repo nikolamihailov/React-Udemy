126678f1a161be6660f25bfb51bf134fbf6e3d77
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-06 19:47:42 +0200

    improving the cart overview and adding helper functions to cart slice

--- 12. Fast React Pizza/src/features/cart/cartSlice.js
+++ 12. Fast React Pizza/src/features/cart/cartSlice.js
@@ -33,3 +33,9 @@ const cartSlice = createSlice({
 export const { addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+    state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+    state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
