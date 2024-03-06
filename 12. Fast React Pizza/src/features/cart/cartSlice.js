40e921bb770b84664a8a5dd975ff92b33b108c31
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-06 19:26:21 +0200

    created cart slice and added it to the store

---
+++ 12. Fast React Pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cart: [],
+};
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addItem(state, action) {
+            state.cart.push(action.payload);
+        },
+        deleteItem(state, action) {
+            state.cart = state.cart.filter(item => item.pizzaId !== action.payload);
+        },
+        increaseItemQuantity(state, action) {
+            const itemIdx = state.cart.findIndex(item => item.pizzaId === action.payload);
+            state.cart[itemIdx].quantity++;
+            state.cart[itemIdx].totalPrice = state.cart[itemIdx].quantity * state.cart[itemIdx].unitPrice;
+        },
+        decreaseItemQuantity(state, action) {
+            const itemIdx = state.cart.findIndex(item => item.pizzaId === action.payload);
+            state.cart[itemIdx].quantity--;
+            state.cart[itemIdx].totalPrice = state.cart[itemIdx].quantity * state.cart[itemIdx].unitPrice;
+        },
+        clearCart(state) {
+            state.cart = [];
+        }
+    }
+});
+
+export const { addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- 12. Fast React Pizza/src/store.js
+++ 12. Fast React Pizza/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./features/user/userSlice";
+import userReducer from "./features/user/userSlice";
+import cartReducer from "./features/cart/cartSlice";
 
 const store = configureStore({
     reducer: {
-        user: userSlice
+        user: userReducer,
+        cart: cartReducer
     }
 });
 
