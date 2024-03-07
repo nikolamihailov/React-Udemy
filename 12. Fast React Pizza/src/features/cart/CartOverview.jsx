0da2d672adfc08bc0c467c2cf64943efd03b8533
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-07 13:36:07 +0200

    small detail about cart overview quantity added

--- 12. Fast React Pizza/src/features/cart/CartOverview.jsx
+++ 12. Fast React Pizza/src/features/cart/CartOverview.jsx
@@ -12,7 +12,9 @@ function CartOverview() {
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>{totalCartQuantity} pizzas</span>
+        <span>
+          {totalCartQuantity} {totalCartQuantity === 1 ? "pizza" : "pizzas"}
+        </span>
         <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
