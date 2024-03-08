4796b0f76091607a6ef893d3b609a6166addb4a3
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-08 20:14:48 +0200

    section finished

--- 12. Fast React Pizza/src/App.jsx
+++ 12. Fast React Pizza/src/App.jsx
@@ -6,6 +6,7 @@ import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./UI/AppLayout";
 
@@ -38,6 +39,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
