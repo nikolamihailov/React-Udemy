3fea7b9fef60c21285f7b5dd569066cc45ef5e6e
Nikola Mihaylov <65945888+nikolamihailov@users.noreply.github.com>
2024-03-08 18:48:41 +0200

    fixed wrong key

--- 12. Fast React Pizza/src/features/order/Order.jsx
+++ 12. Fast React Pizza/src/features/order/Order.jsx
@@ -54,7 +54,7 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
